fa1efb5b9af1501f255257ea567835d25818e35f
yuitotoro0725 <36657349+yuitotoro0725@users.noreply.github.com>
2018-02-26 07:46:30 +0000

    Update face.py

--- face.py
+++ face.py
@@ -1,2 +1,14 @@
-moji = 'Hello world yuiyui'
-print(moji)
+import cognitive_face as CF
+
+KEY = 'e667e6730e524d789b38294418b73cba'  # Replace with a valid subscription key (keeping the quotes in place).
+CF.Key.set(KEY)
+# If you need to, you can change your base API url with:
+#CF.BaseUrl.set('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/')
+
+BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+CF.BaseUrl.set(BASE_URL)
+
+# You can use this example JPG or replace the URL below with your own URL to a JPEG image.
+img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+faces = CF.face.detect(img_url)
+print(faces)

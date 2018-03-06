a4a8fb90601de06b32cf086a47e5428c7dd66436
yuitotoro0725 <36657349+yuitotoro0725@users.noreply.github.com>
2018-03-06 05:44:38 +0000

    Update face.py

--- face.py
+++ face.py
@@ -7,7 +7,8 @@ CF.Key.set(KEY)
 # If you need to, you can change your base API url with:
 #CF.BaseUrl.set('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/')
 
-BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+BASE_URL = 'https://eastasia.api.cognitive.microsoft.com/face/v1.0/'
+# Replace with your regional Base URL
 CF.BaseUrl.set(BASE_URL)
 
 # You can use this example JPG or replace the URL below with your own URL to a JPEG image.
